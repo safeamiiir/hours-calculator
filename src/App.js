4700cb0d880a25a077faafbfc40171dd026dfdb3
Amirreza safehian <amirreza@RIV-D421TWWQ9V.local>
2023-11-13 15:57:52 +0000

    fix: add general functionality

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { Button, Container, InputAdornment, Paper, TextField, Typography } from '@mui/material';
+import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
+import { useState } from 'react';
+
+//This function is ChatGPT created :D
+function secondsToHoursMinutesSeconds(seconds) {
+  seconds = Number(seconds)
+  if (isNaN(seconds) || Number(seconds) < 0) return {
+    hours: '-',
+    minutes: '-',
+    seconds: '-'
+  }
+
+  // Calculate hours
+  const hours = Math.floor(seconds / 3600);
+  // Calculate remaining minutes
+  const remainingMinutes = Math.floor((seconds % 3600) / 60);
+  // Calculate remaining seconds
+  const remainingSeconds = seconds % 60;
+
+  return {
+    hours,
+    minutes: remainingMinutes,
+    seconds: remainingSeconds
+  };
+}
+
+function App() {
+
+  const [seconds, setSeconds] = useState(0)
+  const [time, setTime] = useState(
+    {
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  )
+
+  return (
+    <Container maxWidth="sm" sx={{ p: 2 }}>
+      <Paper sx={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'column', px: 3 }}>
+        <Typography sx={{ my: 3 }}>Hours calculator</Typography>
+        <TextField
+          label="minutes here"
+          id="outlined-start-adornment"
+          sx={{ mb: 2, width: '25ch' }}
+          InputProps={{
+            startAdornment: <InputAdornment position="start"><QueryBuilderIcon /></InputAdornment>,
+          }}
+          value={seconds}
+          onChange={(e) => setSeconds(e.target.value)}
+        />
+        <Button variant="outlined" size="small" onClick={() => setTime(secondsToHoursMinutesSeconds(seconds))}>
+          Convert
+        </Button>
+        <Typography sx={{ my: 2 }}>{`${time.hours} hours, ${time.minutes} minutes, and ${time.seconds} seconds.`}</Typography>
+      </Paper>
+    </Container>
+  );
+}
+
+export default App;

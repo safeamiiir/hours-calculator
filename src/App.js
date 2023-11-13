38b288bab533785ba3ebea14c3f7a6e3a6ca3510
Amirreza safehian <amirreza@RIV-D421TWWQ9V.local>
2023-11-13 16:42:24 +0000

    fix change icon into min

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Button, Container, InputAdornment, Paper, TextField, Typography } from '@mui/material';
-import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
 import { useState } from 'react';
 
 //This function is ChatGPT created :D
@@ -45,7 +44,7 @@ function App() {
           id="outlined-start-adornment"
           sx={{ mb: 2, width: '25ch' }}
           InputProps={{
-            endAdornment: <InputAdornment position="start"><QueryBuilderIcon /></InputAdornment>,
+            endAdornment: <InputAdornment position="start">min</InputAdornment>,
           }}
           value={seconds}
           onChange={(e) => setSeconds(e.target.value)}
